b8a2f865fd27265e0af8c98b0a5f6baf47b63468
akshat1032 <akshat.30oct@gmail.com>
2020-09-22 21:26:39 +0530

    Employee Attendance checked

--- JavaEWP/src/javaewp/JavaEWP.java
+++ JavaEWP/src/javaewp/JavaEWP.java
@@ -1,7 +1,17 @@
 package javaewp;
 
 public class JavaEWP {
-	public static void main(String[] args) {
-		System.out.println("Welcome to Employee Wage Computation Program");
+public static void main(String[] args) {
+		
+		//storing attendance
+		int empAttendance = (int)(Math.floor(Math.random() * 10) % 2);
+		
+		//checking
+		if (empAttendance == 1) {
+			System.out.println("Employee present");
+		}
+		else {
+			System.out.println("Employee absent");
+		}
 	}
 }
