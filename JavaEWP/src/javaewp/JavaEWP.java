aabda4c5d223a1f271c6c833f264824165bbe001
akshat1032 <akshat.30oct@gmail.com>
2020-09-22 21:34:24 +0530

    Employee Daily Wage Calculated

--- JavaEWP/src/javaewp/JavaEWP.java
+++ JavaEWP/src/javaewp/JavaEWP.java
@@ -1,17 +1,28 @@
 package javaewp;
 
 public class JavaEWP {
-public static void main(String[] args) {
+	//constant
+		public static final int WAGE_PER_HOUR = 20;
+		public static final int HOUR_PER_DAY = 8;
 		
-		//storing attendance
-		int empAttendance = (int)(Math.floor(Math.random() * 10) % 2);
-		
-		//checking
-		if (empAttendance == 1) {
-			System.out.println("Employee present");
+		public static void main(String[] args) {
+			
+			//variable
+			double dailyWage = 0;
+			
+			//storing attendance
+			int empAttendance = (int)(Math.floor(Math.random() * 10) % 2);
+			
+			//checking
+			if (empAttendance == 1) {
+				System.out.println("Employee present");
+				dailyWage = WAGE_PER_HOUR * HOUR_PER_DAY;
+			}
+			else {
+				System.out.println("Employee absent");
+			}
+			
+			System.out.println("Daily Wage : "+dailyWage);
 		}
-		else {
-			System.out.println("Employee absent");
-		}
-	}
+
 }
