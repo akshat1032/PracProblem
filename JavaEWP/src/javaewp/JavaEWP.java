88a7319dd5a50eedcb1db1c52b8b3e5371865d02
akshat1032 <akshat.30oct@gmail.com>
2020-09-22 22:19:22 +0530

    Conditional Monthly Wage Calculated

--- JavaEWP/src/javaewp/JavaEWP.java
+++ JavaEWP/src/javaewp/JavaEWP.java
@@ -9,6 +9,7 @@ public class JavaEWP {
 		public static final int IS_FULL_TIME = 0;
 		public static final int IS_PART_TIME = 1;
 		public static final int WORKING_DAY_PER_MONTH = 20;
+		public static final int MAX_WORKING_HOUR = 100;
 		
 		public static void main(String[] args) {
 			
@@ -17,8 +18,11 @@ public class JavaEWP {
 			double partTimeWage = 0;
 			double monthlyWage = 0;
 			double partTimeMonthlyWage = 0;
+			int workingDays = 0;
+			int workingHours = 0;
 			
-			for(int i = 0; i<20; i++) {
+			//loop for checking the required condition
+			while (workingDays <= 20 && workingHours <=100) {
 				
 			//storing attendance
 			int empAttendance = (int)(Math.floor(Math.random() * 10) % 2);
@@ -39,12 +43,16 @@ public class JavaEWP {
 						partTimeMonthlyWage = partTimeMonthlyWage + partTimeWage;
 						break;
 					default:
+						System.out.println("Bad Data");
 						continue;	
 				}
 			}
 			else {
 				System.out.println("Employee absent");
 				}
+			workingDays = workingDays + 1;
+			workingHours = workingHours + 8; //maximum daily hour is assumed to be 8
+			
 			}
 			System.out.println("Full Time Employee Monthly Wage : "+monthlyWage);
 			System.out.println("Part Time Employee Monthly Wage : "+partTimeMonthlyWage);
