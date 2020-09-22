107a426a69cc89c3e388ec52ae7a998433bd72b5
akshat1032 <akshat.30oct@gmail.com>
2020-09-22 21:56:29 +0530

    Switch Case statement calculation

--- JavaEWP/src/javaewp/JavaEWP.java
+++ JavaEWP/src/javaewp/JavaEWP.java
@@ -1,12 +1,14 @@
 package javaewp;
 
 public class JavaEWP {
-	//constant
+	
+		//constant
 		public static final int WAGE_PER_HOUR = 20;
 		public static final int HOUR_PER_DAY = 8;
 		public static final int PART_HOUR_PER_DAY = 8;
 		public static final int IS_FULL_TIME = 0;
 		public static final int IS_PART_TIME = 1;
+		
 		public static void main(String[] args) {
 			
 			//variable
@@ -20,13 +22,17 @@ public class JavaEWP {
 			if (empAttendance == 1) {
 				System.out.println("Employee present");
 				int empCheck = (int)(Math.floor(Math.random() * 10) % 2);
-				if (empCheck == IS_PART_TIME) {
-					partTimeWage = WAGE_PER_HOUR * PART_HOUR_PER_DAY;
-					System.out.println("Part Time Employee Daily Wage : "+partTimeWage);
-				}
-				else if (empCheck == IS_FULL_TIME){
-					dailyWage = WAGE_PER_HOUR * HOUR_PER_DAY;
-					System.out.println("Full Time Employee Daily Wage : "+dailyWage);
+				switch(empCheck) {
+					case IS_FULL_TIME:
+						dailyWage = WAGE_PER_HOUR * HOUR_PER_DAY;
+						System.out.println("Full Time Employee Daily Wage : "+dailyWage);
+						break;
+					case IS_PART_TIME:
+						partTimeWage = WAGE_PER_HOUR * PART_HOUR_PER_DAY;
+						System.out.println("Part Time Employee Daily Wage : "+partTimeWage);
+						break;
+					default:
+						System.out.println("Insufficient data");	
 				}
 			}
 			else {
