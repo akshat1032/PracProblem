2c4cd9003b6f89c9e9a8b3fee959d08ae648dba7
akshat1032 <akshat.30oct@gmail.com>
2020-09-22 21:42:33 +0530

    Part Time Employee Wage Calculated

--- JavaEWP/src/javaewp/JavaEWP.java
+++ JavaEWP/src/javaewp/JavaEWP.java
@@ -4,11 +4,14 @@ public class JavaEWP {
 	//constant
 		public static final int WAGE_PER_HOUR = 20;
 		public static final int HOUR_PER_DAY = 8;
-		
+		public static final int PART_HOUR_PER_DAY = 8;
+		public static final int IS_FULL_TIME = 0;
+		public static final int IS_PART_TIME = 1;
 		public static void main(String[] args) {
 			
 			//variable
 			double dailyWage = 0;
+			double partTimeWage = 0;
 			
 			//storing attendance
 			int empAttendance = (int)(Math.floor(Math.random() * 10) % 2);
@@ -16,13 +19,18 @@ public class JavaEWP {
 			//checking
 			if (empAttendance == 1) {
 				System.out.println("Employee present");
-				dailyWage = WAGE_PER_HOUR * HOUR_PER_DAY;
+				int empCheck = (int)(Math.floor(Math.random() * 10) % 2);
+				if (empCheck == IS_PART_TIME) {
+					partTimeWage = WAGE_PER_HOUR * PART_HOUR_PER_DAY;
+					System.out.println("Part Time Employee Daily Wage : "+partTimeWage);
+				}
+				else if (empCheck == IS_FULL_TIME){
+					dailyWage = WAGE_PER_HOUR * HOUR_PER_DAY;
+					System.out.println("Full Time Employee Daily Wage : "+dailyWage);
+				}
 			}
 			else {
 				System.out.println("Employee absent");
 			}
-			
-			System.out.println("Daily Wage : "+dailyWage);
 		}
-
 }
